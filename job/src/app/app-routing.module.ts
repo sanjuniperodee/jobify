f8c195686b7d7e328b5f1063180c5451b378cbe9
Rinat Kopbaev <rinat.kopbaev@mail.ru>
2023-07-18 17:59:57 +0600

    registr

--- job/src/app/app-routing.module.ts
+++ job/src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { HomeComponent } from './home/home.component';
 import { JobComponent } from './shared/jobh/job.component';
 import { GetjComponent } from './get job/getj.component';
 import { OfferComponent } from './vacansis/offer.component';
+import { RegistrationComponent } from './artist registration/registration';
+import { CreatingComponent } from './creating an application/creating.component';
 
 const routes: Routes = [
       { path: '', component: HomeComponent },
       { path:'about', component: AboutComponent},
       { path:'job', component: GetjComponent},
-      { path: 'offer', component: OfferComponent }
+      { path: 'offer', component: OfferComponent },
+      {path:'registration', component: RegistrationComponent},
+      {path:'creating', component:CreatingComponent}
 ];
 
 @NgModule({
---
+++ job/src/app/creating an application/creating.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creating',
+  templateUrl: './creating.component.html',
+  styleUrls: ['./creating.component.css']
+})
+export class CreatingComponent {
+
+
+}
